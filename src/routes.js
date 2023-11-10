0fad2ef527d35a34749430c4fbf3a1f05df30a35
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-11-10 00:33:02 -0300

    feat: Implement routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,27 @@
-import { buildRoutePathRegExp } from "./utils/build-route-path.js";
+import { randomUUID } from 'node:crypto'
+import { buildRoutePathRegExp } from './utils/build-route-path.js'
+import { Database } from './database.js'
+
+const database = new Database()
 
 export const routes = [
   {
     method: 'POST',
     path: buildRoutePathRegExp('/tasks'),
     handler: (req, res) => {
+      const { title, description } = req.body
+
+      const now = new Date(Date.now()).toISOString()
+      
+      database.insert('tasks', {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: now,
+        updated_at: now,
+      })
+
       return res.writeHead(201).end()
     }
   },
@@ -12,13 +29,46 @@ export const routes = [
     method: 'GET',
     path: buildRoutePathRegExp('/tasks'),
     handler: (req, res) => {
-      return res.writeHead(201).end()
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search,
+      } : null)
+      
+      return res.writeHead(200).end(JSON.stringify(tasks))
     }
   },
   {
     method: 'PUT',
     path: buildRoutePathRegExp('/tasks/:id'),
     handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body  
+
+      const tasks = database.select('tasks', { id: id })
+
+      if (tasks.length === 0) {
+        return res.writeHead(404).end()
+      }
+
+      const task = tasks[0]
+
+      const updatedTask = {
+        ...task,
+        updated_at: new Date(Date.now()).toISOString()
+      }
+
+      if (title) {
+        updatedTask.title = title
+      }
+      
+      if (description) {
+        updatedTask.description = description
+      }
+
+      database.update('tasks', id, updatedTask)
+
       return res.writeHead(201).end()
     }
   },
@@ -26,6 +76,10 @@ export const routes = [
     method: 'DELETE',
     path: buildRoutePathRegExp('/tasks/:id'),
     handler: (req, res) => {
+      const { id } = req.params
+      
+      database.delete('tasks', id)
+
       return res.writeHead(201).end()
     }
   },
@@ -33,6 +87,21 @@ export const routes = [
     method: 'PATCH',
     path: buildRoutePathRegExp('/tasks/:id/complete'),
     handler: (req, res) => {
+      const { id } = req.params
+
+      const tasks = database.select('tasks', { id: id })
+
+      if (tasks.length === 0) {
+        return res.writeHead(404).end()
+      }
+
+      const task = tasks[0]
+
+      database.update('tasks', id, {
+        ...task,
+        completed_at: task.completed_at ? null : new Date(Date.now()).toISOString(),
+      })
+
       return res.writeHead(201).end()
     }
   }
